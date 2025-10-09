882c1988a26c8fccac351ab4877e5fa31b63f800
Gittemberg <karim.guettech@edu.unife.it>
2025-10-09 15:20:44 +0200

    simplified code

--- Condizionale/annoBisestile.c
+++ Condizionale/annoBisestile.c
@@ -4,23 +4,19 @@ int main()
     int a, m, c;
     printf("inserisci l'anno\n");
     scanf("%d%d", &a, &m);
-    if ((a % 4 == 0 && a % 100 != 0) || a % 400 == 0)
-    {
-        printf("è bisestile\n");
-        c = 1;
-    }
-    else
-
-    {
-        printf("non è bisestile\n");
-        c = 0;
-    }
 
     if (m == 2)
-        if (c == 1)
-            printf("29 giorni\n");
+        if ((a % 4 == 0 && a % 100 != 0) || a % 400 == 0)
+        {
+            printf("è bisestile\n");
+            printf("29 giorni");
+        }
         else
-            printf("28 giorni\n");
+
+        {
+            printf("non è bisestile\n");
+            printf("28 giorni");
+        }
     else if (m == 4 || m == 6 || m == 9 || m == 11)
     {
         printf("30 giorni\n");
