5faea331807777d35d985ff4676ca8089addacfb
Gittemberg <karim.guettech@edu.unife.it>
2025-10-09 16:00:21 +0200

    simplified code

--- Condizionale/giornoSuccessivo.c
+++ Condizionale/giornoSuccessivo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int a, m, g;
+    int a, m, g, giorni_mese;
     printf("inserisci giorno mese e anno\n");
 etichetta:
     scanf("%d%d%d", &g, &m, &a);
@@ -14,57 +14,22 @@ etichetta:
     }
     if (m == 2)
         if ((a % 4 == 0 && a % 100 != 0) || a % 400 == 0)
-        {
-            if (g > 29)
-            {
-                g = 1;
-                m = m + 1;
-                if (m > 12)
-                    a = a + 1;
-                m = 1;
-            }
-            printf("%d, %d %d", g, m, a);
-        }
+            giorni_mese = 29;
+
         else
-        {
-            if (g > 28)
-            {
-                g = 1;
-                m = m + 1;
-                if (m > 12)
-                    m = 1;
-                a = a + 1;
-            }
-            printf("%d, %d %d", g, m, a);
-        }
+
+            giorni_mese = 28;
+
     else if (m == 4 || m == 6 || m == 9 || m == 11)
-    {
+        giorni_mese = 30;
 
-        if (g > 30)
-        {
-            g = 1;
-            m = m + 1;
-            if (m > 12)
-            {
-                m = 1;
-                a = a + 1;
-            }
-        }
-        printf("%d, %d %d", g, m, a);
-    }
-    else
+    if (g > giorni_mese)
     {
-
-        if (g > 31)
-        {
-            g = 1;
-            m = m + 1;
-            if (m > 12)
-            {
-                m = 1;
-                a = a + 1;
-            }
-        }
-        printf("%d, %d %d", g, m, a);
+        g = 1;
+        m = m + 1;
+        if (m > 12)
+            a = a + 1;
+        m = 1;
     }
+    printf("%d, %d %d", g, m, a);
 }
